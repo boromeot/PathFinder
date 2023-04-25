d615c8de238fab5ca5ed7d3f55882e9ebb4a0963
boromeot <boromeotyler@gmail.com>
2023-04-25 13:32:14 -0700

    created color map for cells, number to color

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,35 @@ const App: React.FC = () => {
 
   const [gridArr, setGridArr] = useState(new Array());
 
+  const map: {[key: number]: string} = {
+    0 : 'whitesmoke', // Unsearched
+    1 : 'purple', // Searched
+    2 : 'green', // Start
+    3 : 'black', // Wall
+    4 : 'red' // End
+  }
+
   useEffect(() => {
     // Create 2d matrix that the search alogrithms will work on
     const tempArr: Array<Array<Number>> = new Array();
     for (let i = 0; i < COLS; i++) {
-      tempArr.push(new Array(ROWS).fill(0));
+      tempArr.push(new Array(ROWS).fill(2));
     }
     setGridArr(tempArr);
   }, [])
 
+  function handleClick(i: number, j: number) {
+    const newGrid = gridArr.slice();
+    newGrid[i][j] = 1;
+    setGridArr(newGrid);
+  }
+
   return (
     <div className="grid">
-      {gridArr.map((col) => {
-        return <div className="col">
-            {col.map(() => {
-              return <div className="cell"></div>
+      {gridArr.map((col: Array<number>, i: number) => {
+        return <div className="col" key={i}>
+            {col.map((cellValue: number, j: number) => {
+              return <div className={`cell`} style={{backgroundColor: map[cellValue]}} onClick={() => handleClick(i, j)} key={`${i}-${j}`} />
             })}
         </div>
       })}
