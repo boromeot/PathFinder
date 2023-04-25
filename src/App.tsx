3eb75b80ff9dab8559aeb26a5571b1fa63e731a4
boromeot <boromeotyler@gmail.com>
2023-04-25 12:28:17 -0700

    created matrix that the pathfinding alogirthms will work through

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import './App.css';
 
 const App: React.FC = () => {
+  // Grid constants
+  const ROWS: number = 25;
+  const COLS: number = 75;
+
+  const [gridArr, setGridArr] = useState(new Array());
+
+
+  useEffect(() => {
+    // Create 2d matrix that the search alogrithms will work on
+    const tempArr: Array<Array<Number>> = new Array();
+    for (let i = 0; i < ROWS; i++) {
+      tempArr.push(new Array(COLS).fill(0));
+    }
+    setGridArr(tempArr);
+  }, [])
+
   return (
-    <div></div>
+    <div>hello</div>
   )
 };
 
